9569045a7d37ba350759f75ae9c310e1f1241a56
Vinicius Caetano <viniciusgcae@gmail.com>
2022-12-16 09:25:45 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
 
 function App() {
+  const URL_KEY = "da6fc6b470d74ecda37172111221312 ";
+  const city = "Sao paulo";
+  const days = 1;
+  const url = `http://api.weatherapi.com/v1/forecast.json?key=${URL_KEY}&q=${city}&days=${days}&aqi=no&alerts=no
+  `;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p>Dallas</p>
+          </div>
+          <div className="temp">
+            <h1>30°C</h1>
+          </div>
+          <div className="description">
+            <p>Clouds</p>
+          </div>
+        </div>
+        <div className="bottom">
+          <div className="feels">
+            <p>30°C</p>
+          </div>
+          <div className="humidity">
+            <h1>20%</h1>
+          </div>
+          <div className="wind">
+            <h1>20kph</h1>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
